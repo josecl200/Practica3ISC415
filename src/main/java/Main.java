2c9bc0cb335d45441d69fb489be206cd2d3fe477
JRU-001 <j.urena202@live.com>
2019-11-15 18:16:18 -0400

    Marketable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,9 @@ import spark.Spark;
 
 import java.sql.SQLException;
 
-import static spark.Spark.staticFileLocation;
-
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Spark.port(8080);
+        Spark.port(8081);
         Spark.staticFileLocation("/public");
         Bootstrap.startDb();
         Bootstrap.createTables();
